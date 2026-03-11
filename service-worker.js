2cc4d773d0dd0bfdd53ab1009225625e1b77f612
apsgecd <apsgecd@gmail.com>
2026-03-11 14:09:32 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,54 @@
+const CACHE_NAME = "haddad-player-cache-v1";
+
+const ASSETS_TO_CACHE = [
+  "/haddad/",                // root
+  "/haddad/manifest.json",      // manifest
+  "/haddad/service-worker.js",// service worker itself
+  "/haddad/index.html",       // your main page
+  "/haddad/haddad.mp3",        // if you have JS
+  "/haddad/script.js",        // if you have JS
+  "/haddad/icon.png"
+];
+
+// Install event: cache everything
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activate event: remove old caches
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch event: network first, fallback to cache
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        // Save fresh copy to cache
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, clone);
+        });
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
+
+
+
